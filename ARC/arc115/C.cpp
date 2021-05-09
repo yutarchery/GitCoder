d05f1597704c3988679ba36abf3d962723bfdad4
Yuta Suzuki <ytclm13@gmail.com>
2021-05-10 00:38:59 +0900

    [New] Solve arc115c

---
+++ ARC/arc115/C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector < vector<ll> > Mat;
+#define fi first
+#define se second 
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() {printf("yes\n");}
+void no() {printf("no\n");}
+void Yes() {printf("Yes\n");}
+void No() {printf("No\n");}
+void YES() {printf("YES\n");}
+void NO() {printf("NO\n");}
+
+int N;
+
+int countPrime(int num){
+  int ans = 0, nowNum = num;
+  for (int i = 2; i * i <= num; i++){
+    while (nowNum % i == 0){
+      ans++;
+      nowNum /= i;
+    }
+  }
+  if (nowNum > 1) ans++;
+  return ans;
+}
+
+int main(){
+  cin >> N;
+  cout << 1;
+  for (int i = 2; i <= N; i++){
+    cout << " " << countPrime(i) + 1;
+  }
+  cout << endl;
+
+  return 0;
+}
