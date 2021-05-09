7c9b8363dcdceb5e6f76941dff694abc165dfd06
Yuta Suzuki <ytclm13@gmail.com>
2021-05-09 13:13:28 +0900

    [New] Solve abc198b

---
+++ ABC/abc198/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector < vector<ll> > Mat;
+#define fi first
+#define se second 
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() {printf("yes\n");}
+void no() {printf("no\n");}
+void Yes() {printf("Yes\n");}
+void No() {printf("No\n");}
+void YES() {printf("YES\n");}
+void NO() {printf("NO\n");}
+
+int main(){
+  string N; cin >> N;
+
+  int L = N.length();
+  int start = 0, goal = L - 1;
+  while (N[goal] == '0') goal--;
+
+  while (start < goal){
+    if (N[start] == N[goal]){
+      start++; goal--;
+    }else{
+      No();
+      return 0;
+    }
+  }
+  Yes();
+
+  return 0;
+}
