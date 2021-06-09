a01aaad000eb1f5725132d890f0303be06c372a8
Yuta Suzuki <ytclm13@gmail.com>
2021-06-10 00:57:00 +0900

    [Update] Solve abc191b.scala

--- ABC/abc191/abc191b.scala
+++ ABC/abc191/abc191b.scala
@@ -1,25 +1,19 @@
 import java.util.Scanner
+import java.io.PrintWriter
 
 object Main extends App{
     val scanner = new Scanner(System.in)
     val N, X = scanner.nextInt()
 
     var ans = Array[Int]()
+    var flag = false
     for (i <- 0 until N){
         val a = scanner.nextInt()
         if (a != X){
-            ans :+= a
+            if (flag) print(" ")
+            flag = true
+            print(a)
         }
     }
-
-    val K = ans.size
-    for (i <- 0 until K){
-        print(ans(i))
-        if (i < K - 1){
-            print(" ")
-        }else{
-            print("\n")
-        }
-    }
-
+    println()
 }
