99fc8f9a4e57ec412a860479e2d7080aaa0730c1
Yuta Suzuki <ytclm13@gmail.com>
2021-06-17 22:19:50 +0900

    [New] Solve abc201b

---
+++ ABC/abc201/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector < vector<ll> > Mat;
+#define fi first
+#define se second 
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() {printf("yes\n");}
+void no() {printf("no\n");}
+void Yes() {printf("Yes\n");}
+void No() {printf("No\n");}
+void YES() {printf("YES\n");}
+void NO() {printf("NO\n");}
+
+int N;
+pair <int, string> P[1005];
+
+int main(){
+  cin >> N;
+  for (int i = 0; i < N; i++){
+    cin >> P[i].se >> P[i].fi;
+  }
+  sort(P, P + N, greater<pair<int, string> >());
+
+  cout << P[1].se << endl;
+
+  return 0;
+}
