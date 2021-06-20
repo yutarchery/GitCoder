5022576606d914e0e22ee3b5ef5da082c37b4e23
Yuta Suzuki <ytclm13@gmail.com>
2021-06-21 00:08:37 +0900

    [New] Solve abc190d

---
+++ ABC/abc190/D.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector < vector<ll> > Mat;
+#define fi first
+#define se second 
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() {printf("yes\n");}
+void no() {printf("no\n");}
+void Yes() {printf("Yes\n");}
+void No() {printf("No\n");}
+void YES() {printf("YES\n");}
+void NO() {printf("NO\n");}
+
+ll N;
+int ans = 0;
+
+bool check(ll num){
+  return (2 * N / num - num + 1) % 2 == 0;
+}
+
+int main(){
+  cin >> N;
+  for (ll i = 1; i * i <= 2 * N; i++){
+    if ((2 * N) % i != 0) continue;
+    if (check(i)) ans++;
+    if (i * i != 2 * N && check(2 * N / i)) ans++;
+  }
+  cout << ans << endl;
+
+  return 0;
+}
