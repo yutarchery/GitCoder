d30bcdecb26e440f7638f075c2d48b93b250bdc4
Yuta Suzuki <ytclm13@gmail.com>
2021-06-23 23:34:30 +0900

    [New] Solve abc195c

---
+++ ABC/abc195/C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector < vector<ll> > Mat;
+#define fi first
+#define se second 
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() {printf("yes\n");}
+void no() {printf("no\n");}
+void Yes() {printf("Yes\n");}
+void No() {printf("No\n");}
+void YES() {printf("YES\n");}
+void NO() {printf("NO\n");}
+
+ll N, ans, border = 999;
+
+int main(){
+  cin >> N;
+
+  ll cnt = 1;
+  while (border <= N){
+    ll next = border * 1000 + 999;
+    ans += cnt * min(next - border, N - border);
+    cnt++;
+    border = next;
+  }
+  cout << ans << endl;
+
+  return 0;
+}
