04d112f816c9861722eb742f2fdc35d652cd055f
Yuta Suzuki <ytclm13@gmail.com>
2021-06-24 23:11:21 +0900

    [Update] Solve arc122a

--- A.cpp
+++ A.cpp
@@ -29,6 +29,11 @@ int main(){
   cin >> N;
   for (int i = 0; i < N; i++) cin >> A[i];
 
+  if (N == 1){
+    cout << A[0] << endl;
+    return 0;
+  }
+
   DP[1][0] = 1; DP[1][1] = 1;
   for (int i = 2; i < N; i++){
     DP[i][0] = (DP[i - 1][0] + DP[i - 1][1]) % MOD;
