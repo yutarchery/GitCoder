f9c98a81880372752e4b06dfe3082bd0dbc35e6d
Yuta Suzuki <ytclm13@gmail.com>
2021-07-01 23:59:48 +0900

    [Update] Refactoring abc027a.scala

--- ABC/001-050/abc027/A.scala
+++ ABC/001-050/abc027/A.scala
@@ -2,6 +2,6 @@ import java.util.Scanner
 
 object Main extends App {
   val scanner = new Scanner(System.in)
-	val L = Seq.fill(3)(scanner.nextInt()).sortBy(a => a)
-	println(L(0) - L(1) + L(2))
+  val L = Seq.fill(3)(scanner.nextInt())
+  println((L.max + L.min) * 2 - L.sum)
 }
