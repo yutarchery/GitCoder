4ae3e58411482bf4529d5fb182e99c59c5e87701
Yuta Suzuki <ytclm13@gmail.com>
2021-07-07 00:42:12 +0900

    [Update] Solve abc125c.scala

--- ABC/101-150/abc125/C.scala
+++ ABC/101-150/abc125/C.scala
@@ -2,29 +2,25 @@ import scala.io.StdIn._
 
 object Main extends App {
   val n = readInt()
-  val A = readLine().split(" ").map(_.toInt)
+  val aSeq = 0 +: readLine().split(" ").map(_.toInt) :+ 0
 
-  val leftGcd = Array.fill(n)(0)
-  leftGcd(0) = A(0)
-  (1 until n).map { i =>
-    leftGcd(i) = calcGcd(leftGcd(i - 1), A(i))
+  val leftGcd = Array.fill(n + 2)(0)
+  (1 to n).map(i => leftGcd(i) = calcGcd(leftGcd(i - 1), aSeq(i)))
+
+  val rightGcd = Array.fill(n + 2)(0)
+  (1 to n).reverse.map(i => rightGcd(i) = calcGcd(rightGcd(i + 1), aSeq(i)))
+
+  val ans = (1 to n).foldLeft(0) { (res, i) =>
+    math.max(calcGcd(leftGcd(i - 1), rightGcd(i + 1)), res)
   }
 
-  val rightGcd = Array.fill(n)(0)
-  rightGcd(n - 1) = A(n - 1)
-  (0 until (n - 1)).reverse.map { i =>
-    rightGcd(i) = calcGcd(rightGcd(i + 1), A(i))
-  }
-
-  val ans = (1 until (n - 1)).foldLeft(math.max(leftGcd(1), rightGcd(n - 2))) {
-    (res, i) =>
-      math.max(res, calcGcd(leftGcd(i - 1), rightGcd(i + 1)))
-  }
   println(ans)
 
   def calcGcd(a: Int, b: Int): Int = {
     if (a < b) {
       calcGcd(b, a)
+    } else if (b == 0) {
+      a
     } else if (a % b == 0) {
       b
     } else {
