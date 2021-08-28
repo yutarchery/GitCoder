2e3b77c43addd2cdd418d454f2df9c2bfc5c04e1
Yuta Suzuki <ytclm13@gmail.com>
2021-08-28 21:51:16 +0900

    AC abc159d.scala

---
+++ ABC/151-200/abc159/D.scala
@@ -0,0 +1,17 @@
+import scala.io.StdIn._
+
+object Main extends App {
+  val n = readLong()
+  val a = readLine().split(" ").map(_.toInt)
+
+  val count = Array.fill((2e5 + 5).toInt)(0L)
+  a.foreach { ai => count(ai) += 1 }
+
+  val sum = (1 to n.toInt).foldLeft(0L) { (acc, i) =>
+    acc + count(i) * (count(i) - 1) / 2
+  }
+
+  a.foreach { ai =>
+    println(sum - (count(ai) - 1))
+  }
+}
