a9fce86d337bb589c3561c3dfa18a6f9871f805d
Yuta Suzuki <ytclm13@gmail.com>
2021-10-06 09:57:54 +0900

    WA abc220c.scala

---
+++ ABC/201-250/abc220/C.scala
@@ -0,0 +1,21 @@
+import scala.io.StdIn._
+
+object Main extends App {
+  val n = readLong()
+  val a = readLine().split(' ').map(_.toLong)
+  val x = readLong()
+
+  val sum = a.sum
+  println(solve(x, 0, 0))
+
+  def solve(rest: Long, ans: Long, index: Int): Long = {
+    if (rest <= 0) {
+      ans
+    } else if (rest >= sum) {
+      solve(rest % sum, ans + n * (rest / sum), index)
+    } else {
+      solve(rest - a(index), ans + 1, index + 1)
+    }
+  }
+
+}
