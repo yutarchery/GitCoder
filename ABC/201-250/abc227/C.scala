b41c853121069c0cbef62a98c8d1ee1f18f7069c
Yuta Suzuki <ytclm13@gmail.com>
2022-01-13 20:49:33 +0900

    AC abc227c.scala

--- ABC/201-250/abc227/C.scala
+++ ABC/201-250/abc227/C.scala
@@ -3,8 +3,8 @@ import scala.io.StdIn._
 object Main extends App {
   val n = readLong()
 
-  val ans = (1L to math.pow(n, (1.0 / 3.0)).toLong).foldLeft(0L) { (acc, a) =>
-    (a to math.sqrt(n / a).toLong).foldLeft(acc) { (acc2, b) =>
+  val ans = (1L to mySqrt3(n)).foldLeft(0L) { (acc, a) =>
+    (a to mySqrt(n / a)).foldLeft(acc) { (acc2, b) =>
       acc2 + solve(a, b)
     }
   }
@@ -13,5 +13,34 @@ object Main extends App {
 
   def solve(a: Long, b: Long): Long = {
     n / (a * b) - b + 1
+  }
+
+  private def mySqrt(a: Long): Long = binary2(1, 1e6.toLong, a)
+
+  private def binary2(min: Long, max: Long, sum: Long): Long = {
+    if (max - min == 1) {
+      return min
+    }
+
+    val mid = (min + max) / 2
+    if (mid * mid <= sum) {
+      binary2(mid, max, sum)
+    } else {
+      binary2(min, mid, sum)
+    }
+  }
+
+  private def mySqrt3(a: Long): Long = binary3(1, 1e4.toLong, a)
+
+  private def binary3(min: Long, max: Long, sum: Long): Long = {
+    if (max - min == 1) {
+      return min
+    }
+    val mid = (min + max) / 2
+    if (mid * mid * mid <= sum) {
+      binary3(mid, max, sum)
+    } else {
+      binary3(min, mid, sum)
+    }
   }
 }
