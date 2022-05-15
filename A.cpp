26fe26b3735beb6c33134bd4761f0766401d661b
Yuta Suzuki <ytclm13@gmail.com>
2022-05-15 10:57:16 +0900

    Reset to initial a.cpp

--- A.cpp
+++ A.cpp
@@ -22,43 +22,8 @@ void No() { printf("No\n"); }
 void YES() { printf("YES\n"); }
 void NO() { printf("NO\n"); }
 
-ll myPow(ll a, ll n) {
-  if (n == 0) {
-    return 1;
-  } else if (n % 2 == 0) {
-    return myPow(a * a % mod2, n / 2);
-  } else {
-    return a * myPow(a * a % mod2, n / 2) % mod2;
-  }
-}
-
-ll n, d, ans = 0;
-
-void calc(ll numOfParent, ll depth) {
-  ll sum = 0;
-  if (d <= depth) {
-    sum += myPow(2, d + 1);
-  }
-
-  if (depth * 2 >= d) {
-    ll maxJ = min(depth, d - 1), minJ = max((ll)1, d - depth);
-    sum += myPow(2, d - 1) * (maxJ - minJ + 1);
-    sum %= mod2;
-  }
-
-  ans += numOfParent * (sum % mod2);
-  ans %= mod2;
-
-  return;
-}
 
 int main() {
-  cin >> n >> d;
-
-  for (int i = 0; i < n; i++) {
-    calc(myPow(2, i), n - i - 1);
-  }
-  cout << ans << endl;
 
   return 0;
 }
