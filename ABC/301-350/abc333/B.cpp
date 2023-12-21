ba7eb034fc46c2d897f21afd60ca45dffbe1d988
Yuta Suzuki <ytclm13@gmail.com>
2023-12-21 15:35:02 +0900

    Solve ABC333B

---
+++ ABC/301-350/abc333/B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int main() {
+  string s, t;
+  cin >> s;
+  cin >> t;
+
+  int dist_s = min(abs(s[0] - s[1]), 5 - abs(s[0] - s[1]));
+  int dist_t = min(abs(t[0] - t[1]), 5 - abs(t[0] - t[1]));
+
+  if (dist_s == dist_t) {
+    Yes();
+  } else {
+    No();
+  }
+
+  return 0;
+}
