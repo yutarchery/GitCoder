2e72ca5e2c05298db7c582965953b2a28c83645d
Yuta Suzuki <ytclm13@gmail.com>
2024-01-19 14:50:37 +0900

    Update EDPC-I

--- others/Educational_DP/I.cpp
+++ others/Educational_DP/I.cpp
@@ -10,11 +10,11 @@ typedef pair<ll, int> Pli;
 typedef vector<vector<ll>> Mat;
 #define fi first
 #define se second
-const ll mod = 1e9 + 7;
-const ll mod2 = 998244353;
-const ll mod3 = 1812447359;
-const ll inf = 1ll << 62;
-const double pi = 2 * asin(1);
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
 void yes() { printf("yes\n"); }
 void no() { printf("no\n"); }
 void Yes() { printf("Yes\n"); }
@@ -34,20 +34,16 @@ int main() {
   dp[0][0] = 1;
   for (int i = 1; i <= n; i++) {
     for (int j = 0; j <= i; j++) {
-      dp[i][j] += dp[i - 1][j] * (1 - p[i]);
-      if (j - 1 >= 0) {
-        dp[i][j] += dp[i - 1][j - 1] * p[i];
-      }
+      dp[i][j] += dp[i - 1][j] * ((double)1 - p[i]);
+      dp[i][j + 1] += dp[i - 1][j] * p[i];
     }
   }
 
   double ans = 0;
-  for (int i = 1; i <= n; i++) {
-    if (i > (n - i)) {
-      ans += dp[n][i];
-    }
+  for (int j = 0; j < n - j; j++) {
+    ans += dp[n][n - j];
   }
-  cout << setprecision(32) << ans << endl;
+  cout << fixed << setprecision(32) << ans << endl;
 
   return 0;
 }
