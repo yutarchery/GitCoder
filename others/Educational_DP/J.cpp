067d862303e869a40b44c80680c389f3e32356d7
Yuta Suzuki <ytclm13@gmail.com>
2024-01-24 17:14:04 +0900

    Update EDPC-J

--- others/Educational_DP/J.cpp
+++ others/Educational_DP/J.cpp
@@ -10,11 +10,11 @@ typedef pair<ll, int> Pli;
 typedef vector<vector<ll>> Mat;
 #define fi first
 #define se second
-const ll mod = 1e9 + 7;
-const ll mod2 = 998244353;
-const ll mod3 = 1812447359;
-const ll inf = 1ll << 62;
-const double pi = 2 * asin(1);
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
 void yes() { printf("yes\n"); }
 void no() { printf("no\n"); }
 void Yes() { printf("Yes\n"); }
@@ -24,39 +24,37 @@ void NO() { printf("NO\n"); }
 
 int n, a[305], cnt[4];
 
-double p[305][305][305];
+double dp[305][305][305];
 bool visited[305][305][305];
 
 double solve(int i, int j, int k) {
   if (i < 0 || j < 0 || k < 0) {
     return 0;
   }
-  if (visited[i][j][k]) {
-    return p[i][j][k];
-  }
-
-  visited[i][j][k] = true;
-  double ans = 0;
-
   if (i == 0 && j == 0 && k == 0) {
     return 0;
   }
+  if (visited[i][j][k]) {
+    return dp[i][j][k];
+  }
 
-  ans += ((double)i / (double)(i + j + k)) * solve(i - 1, j, k);
-  ans += ((double)j / (double)(i + j + k)) * solve(i + 1, j - 1, k);
-  ans += ((double)k / (double)(i + j + k)) * solve(i, j + 1, k - 1);
-  ans += (double)n / (double)(i + j + k);
+  visited[i][j][k] = true;
 
-  return p[i][j][k] = ans;
+  double ans = (double)(n) / (double)(i + j + k);
+  ans += (double)(i) / (double)(i + j + k) * solve(i - 1, j, k);
+  ans += (double)(j) / (double)(i + j + k) * solve(i + 1, j - 1, k);
+  ans += (double)(k) / (double)(i + j + k) * solve(i, j + 1, k - 1);
+
+  return dp[i][j][k] = ans;
 }
 
 int main() {
   cin >> n;
-  for (int i = 0; i < n; i++) {
+  for (int i = 1; i <= n; i++) {
     cin >> a[i];
     cnt[a[i]]++;
   }
-  cout << setprecision(32) << solve(cnt[1], cnt[2], cnt[3]) << endl;
+  cout << fixed << setprecision(32) << solve(cnt[1], cnt[2], cnt[3]) << endl;
 
   return 0;
 }
