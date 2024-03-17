033cf12432875a57d3e0bb91ce46c10ee8df7d46
Yuta Suzuki <ytclm13@gmail.com>
2024-03-17 11:32:14 +0900

    Solve ABC337B

---
+++ ABC/301-350/abc337/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+string s;
+int now;
+bool flag = true;
+
+int main() {
+  cin >> s;
+  now = s[0] - 'A';
+
+  for (int i = 1; i < s.length(); i++) {
+    if (s[i] == s[i - 1]) {
+      continue;
+    }
+
+    while (now < 3) {
+      now++;
+      if (s[i] == 'A' + now) {
+        break;
+      }
+    }
+
+    if (now == 3) {
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag) {
+    Yes();
+  } else {
+    No();
+  }
+
+  return 0;
+}
