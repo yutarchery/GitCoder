2cb3604e4ebaeef4ee9a366a324c9329a3a3dec6
Yuta Suzuki <ytclm13@gmail.com>
2024-03-21 14:49:21 +0900

    Solve ABC337C

---
+++ ABC/301-350/abc337/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, a[int(3e5 + 5)], b[int(3e5 + 5)];
+int start;
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    if (a[i] == -1) {
+      start = i;
+    } else {
+      b[a[i]] = i;
+    }
+  }
+
+  cout << start;
+  for (int i = 1; i <= n - 1; i++) {
+    start = b[start];
+    cout << " " << start;
+  }
+  cout << endl;
+
+  return 0;
+}
