d83ba12fce1f42e8e54190a4d7001625c170e3eb
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-07-23 03:55:12 +0000

    Solve ABC360C

---
+++ ABC/351-400/abc360/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+ll a[int(1e5 + 5)], w[int(1e5 + 5)];
+vector<ll> now[int(1e5 + 5)];
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    cin >> w[i];
+    now[a[i]].push_back(w[i]);
+  }
+
+  ll ans = 0;
+  for (int i = 1; i <= n; i++) {
+    if (now[i].size() >= 2) {
+      sort(now[i].begin(), now[i].end());
+      for (int j = 0; j < now[i].size() - 1; j++) {
+        ans += now[i][j];
+      }
+    }
+  }
+  cout << ans << endl;
+
+  return 0;
+}
