f1b1cf5c3cd494e7339b7f04a88eb4ee9d192ff3
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-09-06 10:03:14 +0000

    Solve ABC365C

---
+++ ABC/351-400/abc365/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+ll m, a[int(2e5 + 5)], sum;
+
+int main() {
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    sum += a[i];
+  }
+
+  if (sum <= m) {
+    cout << "infinite" << endl;
+    return 0;
+  }
+
+  ll l = 0, r = 1e16;
+  while (r - l > 1) {
+    ll mid = (l + r) / 2;
+    ll now = 0;
+    for (int i = 1; i <= n; i++) {
+      now += min(mid, a[i]);
+    }
+
+    if (now <= m) {
+      l = mid;
+    } else {
+      r = mid;
+    }
+  }
+  cout << l << endl;
+
+  return 0;
+}
