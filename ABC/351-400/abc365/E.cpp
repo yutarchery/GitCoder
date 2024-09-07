3a7011db41cd69f757a1251dd56bc7d236bdcb92
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-09-07 00:44:38 +0000

    Solve ABC365E

---
+++ ABC/351-400/abc365/E.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+ll a[int(2e5 + 5)];
+
+ll dp[int(2e5 + 5)][30];
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+
+  for (int i = 1; i <= n; i++) {
+    ll num = a[i];
+    for (int j = 0; j < 30; j++) {
+      if (num % 2 == 0) {
+        dp[i][j] = dp[i - 1][j];
+      } else {
+        dp[i][j] = i - dp[i - 1][j];
+      }
+      num /= 2;
+    }
+  }
+
+  ll ans = 0;
+  for (int i = 1; i <= n; i++) {
+    ll now = 1;
+    for (int j = 0; j < 30; j++) {
+      ans += now * dp[i][j];
+      now *= 2;
+    }
+    ans -= a[i];
+  }
+  cout << ans << endl;
+
+  return 0;
+}
