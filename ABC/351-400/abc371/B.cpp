0715848cef02cd0fdc71bbca4e5e6c70410e8f37
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-09-21 07:31:46 +0000

    Solve ABC371B

---
+++ ABC/351-400/abc371/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, m;
+pair<int, char> p[105];
+
+bool visited[105];
+
+int main() {
+  cin >> n >> m;
+  for (int i = 1; i <= m; i++) {
+    cin >> p[i].first >> p[i].second;
+
+    if (!visited[p[i].first] && p[i].second == 'M') {
+      visited[p[i].first] = true;
+      Yes();
+    } else {
+      No();
+    }
+  }
+
+  return 0;
+}
