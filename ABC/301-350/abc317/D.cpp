5617d11ccfc96539372a1be8c9b445d3b529a357
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-10-10 01:42:23 +0000

    Solve ABC317D

---
+++ ABC/301-350/abc317/D.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+ll x[105], y[105], z[105];
+
+Pll p[105];
+ll sum, dp[105][int(1e5 + 5)];
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> x[i] >> y[i] >> z[i];
+    p[i] = {z[i], max(0ll, (x[i] + y[i] + 1) / 2 - x[i])};
+    sum += z[i];
+  }
+
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= int(1e5); j++) {
+      dp[i][j] = ll(1e16);
+    }
+  }
+  dp[0][0] = 0;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= int(1e5); j++) {
+      if (dp[i - 1][j] == ll(1e16)) {
+        continue;
+      }
+
+      dp[i][j] = min(dp[i - 1][j], dp[i][j]);
+      dp[i][j + p[i].first] =
+          min(dp[i - 1][j] + p[i].second, dp[i][j + p[i].first]);
+    }
+  }
+
+  ll ans = 1e16;
+  for (ll j = (sum + 1) / 2; j <= sum; j++) {
+    ans = min(ans, dp[n][j]);
+  }
+  cout << ans << endl;
+
+  return 0;
+}
