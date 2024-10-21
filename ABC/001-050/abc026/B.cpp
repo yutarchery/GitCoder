bcc0485972941128e64350ad38cad69e334d355c
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-10-21 07:22:04 +0000

    Solve ABC026B

---
+++ ABC/001-050/abc026/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, r[1005];
+double sum;
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> r[i];
+  }
+  sort(r + 1, r + n + 1, greater<int>());
+
+  for (int i = 1; i <= n; i++) {
+    if (i % 2 == 1) {
+      sum += r[i] * r[i];
+    } else {
+      sum -= r[i] * r[i];
+    }
+  }
+  cout << fixed << setprecision(10) << sum * PI << endl;
+
+  return 0;
+}
