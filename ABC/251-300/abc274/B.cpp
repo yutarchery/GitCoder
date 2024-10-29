32e7bb2c5d4471034803c019c76e0e95a1ef7731
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-10-29 05:05:39 +0000

    Solve ABC274B

---
+++ ABC/251-300/abc274/B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int h, w, ans[1005];
+char c[1005][1005];
+
+int main() {
+  cin >> h >> w;
+  for (int i = 1; i <= h; i++) {
+    for (int j = 1; j <= w; j++) {
+      cin >> c[i][j];
+      if (c[i][j] == '#') {
+        ans[j]++;
+      }
+    }
+  }
+
+  for (int j = 1; j <= w; j++) {
+    cout << ans[j];
+    if (j == w) {
+      cout << endl;
+    } else {
+      cout << " ";
+    }
+  }
+
+  return 0;
+}
