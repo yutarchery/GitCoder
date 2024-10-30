8fef93732599b79c6b9fcbcf5d363122cbfcfd36
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-10-30 02:40:04 +0000

    Solve ABC301D

---
+++ ABC/301-350/abc301/D.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+string s;
+ll n;
+ll ans = 0;
+
+int main() {
+  cin >> s;
+  cin >> n;
+
+  for (int i = 0; i < int(s.length()); i++) {
+    if (s[i] == '1') {
+      ans += (1ll << (int(s.length()) - (i + 1)));
+    }
+  }
+
+  if (n < ans) {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  for (int i = 0; i < int(s.length()); i++) {
+    if (s[i] == '?') {
+      ll now = 1ll << (int(s.length()) - (i + 1));
+      if (ans + now <= n) {
+        ans += now;
+      }
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
