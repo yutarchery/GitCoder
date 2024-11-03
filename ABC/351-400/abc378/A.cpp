718e8950e5912ebc1eb353ca23d8eed0ecb24c3f
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2024-11-03 01:28:29 +0000

    Solve ABC378A

---
+++ ABC/351-400/abc378/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int a[10], cnt[10], ans;
+
+int main() {
+  for (int i = 1; i <= 4; i++) {
+    cin >> a[i];
+    cnt[a[i]]++;
+  }
+  for (int i = 1; i <= 4; i++) {
+    ans += cnt[i] / 2;
+  }
+  cout << ans << endl;
+
+  return 0;
+}
