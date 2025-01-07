222b6c2d522736af2ec88dce25f1b392a5383b0e
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-01-07 02:56:25 +0000

    Solve ABC243C

---
+++ ABC/201-250/abc243/C.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, x[int(2e5 + 5)], y[int(2e5 + 5)];
+char s[int(2e5 + 5)];
+
+map<int, vector<pair<int, char>>> loc;
+map<int, bool> visited;
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> x[i] >> y[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+    loc[y[i]].push_back({x[i], s[i]});
+  }
+
+  for (int i = 1; i <= n; i++) {
+    if (visited[y[i]]) {
+      continue;
+    }
+
+    visited[y[i]] = true;
+    sort(loc[y[i]].begin(), loc[y[i]].end());
+
+    for (int j = 0; j + 1 < loc[y[i]].size(); j++) {
+      if (loc[y[i]][j].second == 'R' && loc[y[i]][j + 1].second == 'L') {
+        Yes();
+        return 0;
+      }
+    }
+  }
+  No();
+
+  return 0;
+}
