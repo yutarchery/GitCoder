f85784b208737dc4a731851f04f80579252507a2
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-01-10 00:57:38 +0000

    Solve ABC253B

---
+++ ABC/251-300/abc253/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int h, w, cnt;
+char s[105][105];
+Pii ans[2];
+
+int main() {
+  cin >> h >> w;
+  for (int i = 1; i <= h; i++) {
+    for (int j = 1; j <= w; j++) {
+      cin >> s[i][j];
+
+      if (s[i][j] == 'o') {
+        ans[cnt] = {i, j};
+        cnt++;
+      }
+    }
+  }
+
+  cout << abs(ans[0].first - ans[1].first) + abs(ans[0].second - ans[1].second)
+       << endl;
+
+  return 0;
+}
