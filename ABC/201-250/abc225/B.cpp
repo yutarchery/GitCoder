917b563b39c7735dc6b2066417e6f59483676372
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-01-17 04:30:52 +0000

    Solve ABC225B

---
+++ ABC/201-250/abc225/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, a, b, cnt[int(1e5 + 5)];
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n - 1; i++) {
+    cin >> a >> b;
+    cnt[a]++;
+    cnt[b]++;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    if (cnt[i] == 1 || cnt[i] == n - 1) {
+      continue;
+    }
+    No();
+    return 0;
+  }
+  Yes();
+
+  return 0;
+}
