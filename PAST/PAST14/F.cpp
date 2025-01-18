64f664b152be28a495a13f56b1705cd0f2bb82b4
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-01-18 04:31:24 +0000

    Solve PAST14F

---
+++ PAST/PAST14/F.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, q, m, s[int(2e5 + 5)], t[int(2e5 + 5)], ans[int(2e5 + 5)];
+map<int, bool> visited;
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+    visited[s[i]] = true;
+  }
+
+  cin >> q;
+  for (int i = 1; i <= q; i++) {
+    cin >> m;
+    ans[i] = n;
+    for (int j = 1; j <= m; j++) {
+      cin >> t[j];
+      if (!visited[t[j]]) {
+        ans[i]++;
+      }
+    }
+  }
+
+  for (int i = 1; i <= q; i++) {
+    cout << ans[i] << endl;
+  }
+
+  return 0;
+}
