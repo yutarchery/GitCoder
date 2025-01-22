75a4095685ac9b4cc070c12518ecf5d502b17074
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-01-22 05:49:06 +0000

    Solve PAST15D

---
+++ PAST/PAST15/D.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int a, b, c, d, r, p;
+int cnt[3005];
+
+int main() {
+  cin >> a >> b >> c >> d >> r;
+  p = a;
+
+  for (int i = 0; i < c + r; i += d) {
+    if (i >= b && p == a) {
+      p = c;
+    }
+
+    if (i < p + r) {
+      cnt[max(p, i)]++;
+      cnt[p + r]--;
+    }
+  }
+
+  for (int i = 1; i < c + r; i++) {
+    cnt[i] += cnt[i - 1];
+  }
+  for (int i = c; i < c + r; i++) {
+    if (cnt[i] == 0) {
+      No();
+      return 0;
+    }
+  }
+  Yes();
+
+  return 0;
+}
