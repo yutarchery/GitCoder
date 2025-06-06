7d9990813cc1221ccc3c8a51b4e0d5165d5be338
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-06-06 11:09:05 +0000

    Solve ABC260A

---
+++ ABC/251-300/abc260/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+char s[3];
+int cnt[26];
+
+int main() {
+  for (int i = 0; i < 3; i++) {
+    cin >> s[i];
+    cnt[s[i] - 'a']++;
+  }
+
+  for (int i = 0; i < 26; i++) {
+    if (cnt[i] == 1) {
+      cout << char('a' + i) << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+
+  return 0;
+}
