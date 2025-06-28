56c6880afb3950e7a3c73211dc8eaeac92f7b968
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-06-28 20:46:53 +0900

    Solve ABC407E

---
+++ ABC/401-450/abc407/E.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n;
+ll a[int(4e5 + 5)];
+
+ll solve() {
+  priority_queue<ll> que;
+  ll ans = 0;
+  int cnt = 0;
+
+  for (int i = 1; i <= 2 * n; i++) {
+    que.push(a[i]);
+
+    while (cnt < (i + 1) / 2) {
+      cnt++;
+      ans += que.top();
+      que.pop();
+    }
+
+    if (cnt == n) {
+      break;
+    }
+  }
+
+  return ans;
+}
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    cin >> n;
+    for (int i = 1; i <= 2 * n; i++) {
+      cin >> a[i];
+    }
+    cout << solve() << endl;
+  }
+
+  return 0;
+}
