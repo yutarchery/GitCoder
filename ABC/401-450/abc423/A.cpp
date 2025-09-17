beef778f2ecd2a2428cb2ffbba7bac773c456502
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-09-17 11:32:34 +0900

    Solve ABC423A

---
+++ ABC/401-450/abc423/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+ll x, c;
+
+int main() {
+  cin >> x >> c;
+
+  ll ans = 0;
+  for (ll i = 0; i <= x; i += 1000) {
+    int now = i + i * c / 1000;
+    if (now <= x) {
+      ans = i;
+    }
+  }
+  cout << ans << endl;
+
+  return 0;
+}
