31b61d92aa14b1bc6c6655710ce6e6277ae7fece
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-09-17 11:39:29 +0900

    Solve ABC423B

---
+++ ABC/401-450/abc423/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, l[105];
+bool visited[105];
+
+int main() {
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> l[i];
+  }
+  visited[0] = true;
+  visited[n] = true;
+
+  for (int i = 1; i <= n; i++) {
+    if (l[i] == 1) {
+      break;
+    }
+    visited[i] = true;
+  }
+  for (int i = n; i >= 1; i--) {
+    if (l[i] == 1) {
+      break;
+    }
+    visited[i - 1] = true;
+  }
+
+  int ans = 0;
+  for (int i = 0; i <= n; i++) {
+    if (!visited[i]) {
+      ans++;
+    }
+  }
+  cout << ans << endl;
+
+  return 0;
+}
