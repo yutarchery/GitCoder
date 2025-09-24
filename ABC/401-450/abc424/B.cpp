42075c2241121243bd645be7d290dc8b3143350f
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-09-24 00:32:03 +0000

    Solve ABC424B

---
+++ ABC/401-450/abc424/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int n, m, k, a, b;
+int cnt[15];
+vector<int> ans;
+
+int main() {
+  cin >> n >> m >> k;
+  for (int i = 1; i <= k; i++) {
+    cin >> a >> b;
+    cnt[a]++;
+    if (cnt[a] == m) {
+      ans.push_back(a);
+    }
+  }
+
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << (i == ans.size() - 1 ? '\n' : ' ');
+  }
+
+  return 0;
+}
