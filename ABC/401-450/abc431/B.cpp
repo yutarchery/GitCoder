406e2b8298e3fcef83da0ba0e15b3808f791130c
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2025-11-22 20:57:48 +0900

    Solve ABC431B

---
+++ ABC/401-450/abc431/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { printf("yes\n"); }
+void no() { printf("no\n"); }
+void Yes() { printf("Yes\n"); }
+void No() { printf("No\n"); }
+void YES() { printf("YES\n"); }
+void NO() { printf("NO\n"); }
+
+int x, n, w[105], q, p[105];
+bool visited[105];
+
+int main() {
+  cin >> x;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> w[i];
+  }
+  cin >> q;
+  for (int i = 1; i <= q; i++) {
+    cin >> p[i];
+
+    if (visited[p[i]]) {
+      x -= w[p[i]];
+      visited[p[i]] = false;
+    } else {
+      x += w[p[i]];
+      visited[p[i]] = true;
+    }
+
+    cout << x << endl;
+  }
+
+  return 0;
+}
