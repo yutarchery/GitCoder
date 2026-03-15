be38996e5680ed3e77aabb531c780a1a00d64990
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-03-15 21:12:48 +0900

    Solve PAST22B

---
+++ PAST/PAST22/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int w, r;
+  cin >> w >> r;
+
+  if (r == 1) {
+    cout << "absent\n";
+  } else if (w == 1) {
+    cout << "late\n";
+  } else {
+    cout << "attend\n";
+  }
+
+  return 0;
+}
