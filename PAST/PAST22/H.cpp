473fc14b5c291e603a4dd954e8da9499bae244dc
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-03-15 21:44:46 +0900

    Solve PAST22H

---
+++ PAST/PAST22/H.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, m, l[int(3e5 + 5)], r[int(3e5 + 5)];
+int ans[int(3e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m;
+  for (int i = 1; i <= m; i++) {
+    cin >> l[i] >> r[i];
+    if (l[i] >= r[i]) {
+      swap(l[i], r[i]);
+    }
+
+    ans[l[i]]++;
+    ans[r[i] + 1]--;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    ans[i] += ans[i - 1];
+    cout << ans[i] << (i == n ? '\n' : ' ');
+  }
+
+  return 0;
+}
