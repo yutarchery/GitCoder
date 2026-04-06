71d178e4caf0370474e13fc0162a4c76b6a90308
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-06 11:48:20 +0900

    Solve ABC450D

---
+++ ABC/401-450/abc450/D.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n;
+ll k, a[int(2e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    a[i] %= k;
+  }
+  sort(a + 1, a + n + 1);
+
+  ll ans = a[n] - a[1];
+  for (int i = 1; i + 1 <= n; i++) {
+    ans = min(ans, a[i] + k - a[i + 1]);
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
