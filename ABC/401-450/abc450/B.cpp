24af81baf32f617f866499f9dead3ee0856b8a2f
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-06 11:32:11 +0900

    Solve ABC450B

---
+++ ABC/401-450/abc450/B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n;
+ll c[105][105];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      cin >> c[i][j];
+    }
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      for (int k = j + 1; k <= n; k++) {
+        if (c[i][k] > c[i][j] + c[j][k]) {
+          Yes();
+          return 0;
+        }
+      }
+    }
+  }
+
+  No();
+
+  return 0;
+}
