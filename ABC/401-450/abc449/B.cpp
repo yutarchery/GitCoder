b39825ff7079cdbbe7141450021e6061f7c58b5d
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-07 10:38:49 +0900

    Solve ABC449B

---
+++ ABC/401-450/abc449/B.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int h, w, q, op, r, c;
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> h >> w >> q;
+  while (q--) {
+    cin >> op;
+
+    if (op == 1) {
+      cin >> r;
+      cout << r * w << '\n';
+      h -= r;
+    } else {
+      cin >> c;
+      cout << h * c << '\n';
+      w -= c;
+    }
+  }
+
+  return 0;
+}
