55084c6353fcdfd54d339da14b1a8582a3e5405b
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-04-19 21:47:15 +0900

    Solve ABC454C

---
+++ ABC/451-500/abc454/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, m, a, b;
+vector<int> graph[int(3e5 + 5)];
+
+queue<int> que;
+bool visited[int(3e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m;
+  for (int i = 1; i <= m; i++) {
+    cin >> a >> b;
+    graph[a].emplace_back(b);
+  }
+
+  que.push(1);
+  visited[1] = true;
+  while (!que.empty()) {
+    int q = que.front();
+    que.pop();
+
+    for (int next : graph[q]) {
+      if (!visited[next]) {
+        visited[next] = true;
+        que.push(next);
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+    if (visited[i]) {
+      ans++;
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
