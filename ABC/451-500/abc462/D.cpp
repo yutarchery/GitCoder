d67f41a4591618e59c4bf0868293fbfb5bfdc57c
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-06-15 12:09:45 +0900

    Solve ABC462D

---
+++ ABC/451-500/abc462/D.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, d, s[int(2e5 + 5)], t[int(2e5 + 5)];
+
+priority_queue<Pii, vector<Pii>, greater<Pii>> start;
+priority_queue<int, vector<int>, greater<int>> goal;
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> d;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i] >> t[i];
+    start.push({s[i], t[i]});
+  }
+
+  ll ans = 0;
+  for (int i = 1; i <= int(2e6 + 5); i++) {
+    while (!start.empty()) {
+      Pii q = start.top();
+      if (q.first == i) {
+        goal.push(q.second);
+        start.pop();
+      } else {
+        break;
+      }
+    }
+    while (!goal.empty()) {
+      int q = goal.top();
+      if (q < i + d) {
+        goal.pop();
+      } else {
+        break;
+      }
+    }
+
+    ans += ll(goal.size()) * ll(goal.size() - 1) / 2;
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
