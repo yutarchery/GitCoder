9ec53ea943aad51b7b66fd501fe0a63280971ec1
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-07-01 11:14:00 +0900

    Solve KEYENCE2021A

---
+++ ARC/ARC-Like/KEYENCE2021/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n;
+ll a[int(2e5 + 5)], b[int(2e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    cin >> b[i];
+  }
+
+  ll a_max = 0, ans = 0;
+  for (int i = 1; i <= n; i++) {
+    a_max = max(a_max, a[i]);
+    ans = max(ans, a_max * b[i]);
+    cout << ans << '\n';
+  }
+
+  return 0;
+}
