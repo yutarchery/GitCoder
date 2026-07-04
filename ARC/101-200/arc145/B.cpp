b9d21699cf79c50e7bac306adbb13ea640cebdee
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-07-04 21:10:45 +0900

    Solve ARC145B

---
+++ ARC/101-200/arc145/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+ll n, a, b;
+
+ll solve(ll x) { return (x / a) * min(a, b) + min(x % a, b - 1); }
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> a >> b;
+  cout << max(0ll, solve(n) - solve(a - 1)) << '\n';
+
+  return 0;
+}
