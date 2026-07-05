98025291c8a00814036270c414c9d583a3f667f4
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-07-05 13:41:19 +0900

    Solve ABC465C

---
+++ ABC/451-500/abc465/C.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, a[int(5e5 + 5)];
+char s[int(5e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+  }
+
+  int start = 1, end = n;
+  bool is_reversed = false;
+  for (int i = n; i >= 1; i--) {
+    if (s[i] == 'o') {
+      is_reversed = !is_reversed;
+    }
+
+    if (is_reversed) {
+      a[start] = i;
+      start++;
+    } else {
+      a[end] = i;
+      end--;
+    }
+  }
+
+  for (int i = 1; i <= n; i++) {
+    cout << a[i] << (i == n ? '\n' : ' ');
+  }
+
+  return 0;
+}
