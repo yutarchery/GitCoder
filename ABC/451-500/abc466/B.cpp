42d6cb8711fe268b6e687514739ee22eeddf0d68
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-07-13 18:13:51 +0900

    Solve ABC466B

---
+++ ABC/451-500/abc466/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, m, c[105], s[105];
+int ans[105];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m;
+  fill(ans + 1, ans + m + 1, -1);
+
+  for (int i = 1; i <= n; i++) {
+    cin >> c[i] >> s[i];
+    ans[c[i]] = max(ans[c[i]], s[i]);
+  }
+
+  for (int i = 1; i <= m; i++) {
+    cout << ans[i] << (i == m ? '\n' : ' ');
+  }
+
+  return 0;
+}
