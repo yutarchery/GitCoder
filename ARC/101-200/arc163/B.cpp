e4d4364ff5db4d67d5e28c41d6eee35a5e20937f
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-08-12 12:00:25 +0900

    Solve ARC163B

---
+++ ARC/101-200/arc163/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, m;
+int a[int(2e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  sort(a + 3, a + n + 1);
+
+  int ans = 2e9;
+  for (int i = 3; i + m - 1 <= n; i++) {
+    ans = min(ans, max(0, a[1] - a[i]) + max(0, a[i + m - 1] - a[2]));
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
