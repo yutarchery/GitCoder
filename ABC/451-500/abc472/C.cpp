ef0d633d8e9460d9272bb3dc41813b3085f0fb56
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-08-28 12:17:32 +0900

    Solve ABC472C

---
+++ ABC/451-500/abc472/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, m;
+ll k, a[int(2e5 + 5)];
+
+bool ans[int(2e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> m >> k;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+
+  ll now = 0;
+  for (int i = 1; i <= n; i++) {
+    if (now + a[i] <= k) {
+      now += a[i];
+      ans[i] = true;
+    }
+
+    if (i - m + 1 >= 1 && ans[i - m + 1]) {
+      now -= a[i - m + 1];
+    }
+  }
+
+  for (int i = 1; i <= n; i++) {
+    ans[i] ? Yes() : No();
+  }
+
+  return 0;
+}
