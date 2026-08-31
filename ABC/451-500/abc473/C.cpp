69dd10aff37bb325ac30ce53dfff676fc8c88ead
Yuta Suzuki <35495324+yutarchery@users.noreply.github.com>
2026-08-31 18:56:52 +0900

    Solve ABC473C

---
+++ ABC/451-500/abc473/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> Pii;
+typedef pair<int, ll> Pil;
+typedef pair<ll, ll> Pll;
+typedef pair<ll, int> Pli;
+typedef vector<vector<ll>> Mat;
+#define fi first
+#define se second
+const ll MOD = 1e9 + 7;
+const ll MOD2 = 998244353;
+const ll MOD3 = 1812447359;
+const ll INF = 1ll << 62;
+const double PI = 2 * asin(1);
+void yes() { cout << "yes\n"; }
+void no() { cout << "no\n"; }
+void Yes() { cout << "Yes\n"; }
+void No() { cout << "No\n"; }
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+int n, k, a[int(2e5 + 5)];
+int cnt[int(2e5 + 5)];
+
+int main() {
+  // cin の高速化
+  std::cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cin >> n >> k;
+  int cnt_max = 0;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+    cnt[a[i]]++;
+    cnt_max = max(cnt_max, cnt[a[i]]);
+  }
+
+  int ans = 0;
+  for (int i = 1; i <= k; i++) {
+    if (cnt[i] + 1 >= cnt_max) {
+      ans++;
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
